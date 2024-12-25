21b75b63d3749f51817df0944220dc5787e24ebc
Nikolay Stoynov <arvigeus@gmail.com>
2024-12-25 17:23:06 +0200

    feat: change serif and sans-serif fonts From IBMPlex to Harmonia and ITC Stepp

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,8 +39,8 @@ export default {
         mobileGallery: 'calc(100vw - 3rem)',
       },
       fontFamily: {
-        sans: ['Helvetica Neue', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-        serif: ['"IBMPlexSerif"', 'Palatino', 'ui-serif'],
+        sans: ['Harmonia Sans', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['"ITC Stepp"', 'Palatino', 'ui-serif'],
       },
       fontSize: {
         display: ['var(--font-size-display)', '1.1'],
