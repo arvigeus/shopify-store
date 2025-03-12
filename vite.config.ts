aa6c5853d3c1174bf8d516841f81bb6341e7622a
Nikolay Stoynov <arvigeus@gmail.com>
2025-03-12 12:12:50 +0700

    feat: F5 to launch

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+	server: {
+		open: true,
+	},
 	plugins: [
 		hydrogen(),
 		oxygen(),
